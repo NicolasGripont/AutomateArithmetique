9801afc09edaf3594298fc0733f2792622b6f104
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-02-14 18:05:32 +0100

    Ajout du menu d'interaction utilisateur et du fichier test.txt contenant des expressions arithmetiques

--- main.cpp
+++ main.cpp
@@ -3,9 +3,40 @@
 
 using namespace std;
 
+void test();
+void showMenu();
+
 int main() {
+
+    test();
+    //showMenu();
+
+    return 0;
+}
+
+void test() {
     Automate automate;
     automate.execute("(45+(56+78))*54");
     cout << automate << endl;
-    return 0;
+}
+
+
+void showMenu() {
+    string exit = "exit";
+    string line;
+
+    cout << endl;
+    cout << "Entrez 'exit' pour quitter le programme." << endl;
+
+    do {
+        line = "";
+        cout << "Entrez une expression arithmétique (composée de nombres, '(', ')', '+', '*') :" << endl;
+        getline(cin, line);
+
+        if(!line.empty() && line.compare(exit) != 0) {
+//        TODO : analyser et évaluer l'expression
+            cout << endl;
+        }
+
+    } while (line.compare(exit) != 0);
 }
